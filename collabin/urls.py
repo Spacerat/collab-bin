60e6ac1ab2d9e2b02594083c9358f0488f8447d8
Carl D <catdaemon@gmail.com>
2011-02-01 21:48:46 +0000

    Initial commit.

---
+++ collabin/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^collabin/', include('collabin.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    #(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+     (r'^admin/', include(admin.site.urls)),
+
+     # Include the pastebin url rewriter
+     (r'^pastebin/?', include('collabin.paste.urls')),
+)
