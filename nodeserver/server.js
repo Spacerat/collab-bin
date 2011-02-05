4adc6746283603886c44698bfb17493c446d21b4
Spacerat <spacerat3004@gmail.com>
2011-02-05 22:51:45 +0000

    Packet class, more implimentation.

--- nodeserver/server.js
+++ nodeserver/server.js
@@ -8,16 +8,44 @@ var Room = function() {
     var text;
     var url;
     var clients;
+    var max_clients=10;
+    
+    /* Return the number of places left in the room. */
+    this.getRemainingSpace = function() {
+        return max_clients - clients.length;
+    }
 }
 
-var RejectPacket = function(reason, room) {
-    return {'reject' {
-        'reason': reason,
-        'room_url': room.url
-    }}
+
+/* Packet class. 
+    Example use: client.send(new Packet().acceptJoin().fullText(room).clientList(room).data);
+*/
+var Packet = function() {
+    
+    this.data = {};
+    
+    //reject_join, sent to clients who have been rejected from a room for some reason
+    this.Reject = function(reason,room) {
+        data.reject {};
+        data.reject.reason = reason;
+        if (room) data.reject.room = room;
+        return this;
+    }
+    
+    //accept_token, client's token has been accepted
+    this.acceptToken = function() {
+        data.accept_join = true;
+        return this;
+    }
+    
+    //Send this packet to a client;
+    this.send(client) {
+        client.send(this.data);
+    }
 }
 
-
+/* Server class.
+*/
 this.Server = function(app) {
     var socket = io.listen(app);
     var rooms = {};
@@ -28,34 +56,50 @@ this.Server = function(app) {
     }
     
     socket.on('connection', function(client) {
-            
+        
+        var Disconnect = function() {
+            client.connected = false;
+            client.on('message',undefined); //no clue if this works.
+        }
+        
         //Set data for this new client.
         client.collab = {
             verified: false,
             room: null,
-            name: ""
-            
+            userid: "",
+            name: "",
+            avatar_url: "",            
         }
         
         client.on('message', function(data) {
+            if (!client.connected) return;
+            
             //Client has initiated the handshake procedure
             if ('connect' in data) {
                 if (client.verified == true) {
-                    throw "Client already verified?!";
+                    new Packet().Reject("Illegal attempt to handshake twice.").Send(client);
+                    Disconnect();
+                    return;
                 }
-                //If the token is valid, update the client's details. Reject them otherwise.
+                if (client.collab.room) {
+                    new Packet().Reject("You are already in a room.").Send(client);
+                    Disconnect();
+                    return;
+                }
+                //If the token is valid, update the client's details and attempt to join the room.
                 verify_token(data.token, function(response) {
-                    
+                    if (response.accepted === true) {
+                        client.collab.userid = response.details.userid;
+                        client.collab.name = response.details.name;
+                        client.avatar_url = response.details.avatar_url;
+                        
+                        new Packet().acceptToken().Send(client);
+                    }
+                    else {
+                        new Packet().Reject("Invalid token.").Send(client);
+                    }
                 });
             }
-        
-            //Client has requested to join a room.
-            if ('reject_join' in data) {
-                if (client.collab.room) {
-                    client.send(RejectPacket("You are already in a room."));
-                }
-            }
         });
-        
     });
 }
