0cf164e8e8c097efefbf86ffdf74e5e42560a059
Spacerat <spacerat3004@gmail.com>
2011-02-06 01:36:20 +0000

    Moved the Packet class in to a separate file.

--- nodeserver/server.js
+++ nodeserver/server.js
@@ -1,6 +1,7 @@
 
 var io = require('socket.io');
 var util = require('util');
+var Packet = require('packet').Packet;
 
 /* Room class. Pass settings object in the form: {
     max_clients: int,    //10
@@ -49,77 +50,6 @@ var Room = function(settings) {
 }
 
 
-/* Packet class. 
-    Example use of chaining: new Packet().acceptJoin().fullText(room).clientList(room).Send(client);
-*/
-var Packet = function() {
-    
-    this.data = {};
-    
-    //reject_join, sent to clients who have been rejected from a room for some @reason
-    this.Reject = function(reason, room) {
-        data.reject {};
-        data.reject.reason = reason;
-        if (room) data.reject.room = room;
-        return this;
-    }
-    
-    //accept_token, client's token has been accepted.
-    this.acceptToken = function() {
-        data.accept_token = true;
-        return this;
-    }
-    
-    //new_applicant, inform room members of a new applicant.
-    this.newApplicant = function(client) {
-        data.new_applicant = {
-            details: client.details
-        }
-    }
-    
-    //new_member, inform room members of a new member.
-    this.newMember = function(client) {
-        data.new_member = {
-            details: client.details,
-            can_write: client.can_write
-        }
-    }
-    
-    //accept_join, inform a client that their request to join has been accepted.
-    this.acceptJoin = function(can_write) {
-        data.accept_join = {
-            can_write: can_write;
-        }
-        return this;
-    }
-    
-    ///////////////////////////////
-    //Send this packet to a client;
-    this.Send(client) {
-        client.send(this.data);
-    }
-    
-    
-    //Send this packet to all clients other than @exclude
-    this.Broadcast(socket, exclude) {
-        if (exclude) {
-            socket.broadcast(this.data, exclude.sessionId);
-        }
-        else {
-            socket.broadcast(this.data);
-        }
-    }
-    
-    //Send this packet to all clients in @room other than @exclude
-    this.broadcastToRoom(socket, room, exclude, include_applicants) {
-        var i;
-        var dest = room.members;
-        if (include_applicants===true) {dest=dest.concat(room.applicants);}
-        for (i in dest) {
-            if (dest[i]!==exclude) {dest[i].send(this.data);}
-        }
-    }
-}
 
 /* Server class.
 */
